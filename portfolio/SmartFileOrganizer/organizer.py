6770909aece027df238fa9b00c2656842ea4599c
bnestes18 <bnestes2014@gmail.com>
2026-02-19 14:35:51 -0500

    Refactor code, add dry-run flag

--- portfolio/SmartFileOrganizer/organizer.py
+++ portfolio/SmartFileOrganizer/organizer.py
@@ -10,6 +10,7 @@ import constants as const
 from shutil import move
 import logging
 from datetime import datetime
+import argparse
 
 # ----- CONFIGURATION -----
 
@@ -22,8 +23,6 @@ Must provide absolute path. Edit this line!!!
 """
 source_folder = Path("/Users/estesb1/code/python/python-projects/portfolio/SmartFileOrganizer/files")
 
-DRY_RUN = True
-
 """ 
 Returns category name from given file extension 
 Example .jpg extension -> Images
@@ -34,43 +33,62 @@ def get_category(file_suffix):
             return category  
     return "Others"
 
+def build_target_folder(organized_root: Path, file: Path):
+    category = get_category(file.suffix)
+    
+    # Get the file's modified time as a datetime object
+    modified_time = datetime.fromtimestamp(file.stat().st_mtime)
+    date_folder = modified_time.strftime("%Y-%m")  # e.g., "2025-07"
+
+    # Build the full target path with category and date
+    return organized_root / category / date_folder
+        
+
 # Iterate through source folder
-def organize_files():
+def organize_files(source_folder: Path, output_root: Path, dryrun: bool=False):
     if not source_folder.exists or not source_folder.is_dir():
         logging.error("The specified source folder either does not exist or is not a directory.")
         return
         
     # Create the organized folder structure
-    organized = source_folder / "Organized"
-    organized.mkdir(parents=True, exist_ok=True)
+    # organized = source_folder / "Organized"
+    output_root.mkdir(parents=True, exist_ok=True)
     
     for item in source_folder.iterdir():
         # print(file.name)
         if item.is_file():
-            category = get_category(item.suffix)
-            # TODO Create dry-run mode: List all files moves in a list, loop through list and print each line.
+            # category = get_category(item.suffix)
             
-            # Get the file's modified time as a datetime object
-            mod_time = datetime.fromtimestamp(item.stat().st_mtime)
-            date_folder_name = mod_time.strftime("%Y-%m")  # e.g., "2025-07"
+            # # Get the file's modified time as a datetime object
+            # mod_time = datetime.fromtimestamp(item.stat().st_mtime)
+            # date_folder_name = mod_time.strftime("%Y-%m")  # e.g., "2025-07"
 
-            # Build the full target path with category and date
-            target_folder = organized / category / date_folder_name
+            # # Build the full target path with category and date
+            # target_folder = organized / category / date_folder_name
+            target_folder = build_target_folder(output_root, item)
             target_folder.mkdir(parents=True, exist_ok=True)
             
-            # Define destination path
-            dest_folder = target_folder / item.name
             try:
-                if DRY_RUN:
+                if dryrun:
                     logging.info(f"[DRY RUN MODE] Would move '{item.name}' to '{target_folder}'")
                 else: 
                     # Move files from source to destination folder
+                    dest_folder = target_folder / item.name
                     move(item, dest_folder)
                     logging.info(f"Moving [{item.name}] to [{target_folder}]\n")
+                    logging.info("Organizing complete!")
             except Exception as e:
                 logging.error(f"Error moving '[{item.name}]': {e}\n")
+                return e
 
-organize_files()
+if __name__ == "__main__":
+    logging.info("Starting Smart File Organizer...")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dryrun", action="store_true")
+    args = parser.parse_args()
+    organize_files(dryrun=args.dryrun)
+    
+    
 # TODO Additional Enhancements
 # Configurable rules (optional) (categorize by filetype, modification date or size)
 # Create a GUI (optional)
