c6c1bb7aa3542ad64de8f286ea0b9ffcaaa00f8b
bnestes18 <bnestes2014@gmail.com>
2026-04-02 22:31:14 -0400

    move exception handling into separate function

--- portfolio/ci_timer/api/client.py
+++ portfolio/ci_timer/api/client.py
@@ -40,10 +40,9 @@ class GitHubClient:
             data = response.json()
             return data["workflow_runs"]
         except httpx.HTTPStatusError as e:
-            logging.critical(f"Failed to retrieve workflow runs: {e}")
-        
-        data = response.json()
-        return data["workflow_runs"]
+            utils.handle_http_error(e, "Failed to retrieve workflow runs")
+        except httpx.RequestError as e: 
+            utils.handle_http_error(e, "Network error occurred while retrieving workflow runs")
 
     def get_jobs_for_run(self, owner: str, repo: str, run_id: int) -> list[dict]:
         return self._paginate(f"/repos/{owner}/{repo}/actions/runs/{run_id}/jobs", key="jobs")
@@ -63,6 +62,10 @@ class GitHubClient:
                 # else, use provided key to extract desired response data
                 results.extend(data[key])
                 next_link_url = response.links.get("next", {}).get("url")
+            except httpx.HTTPStatusError as e:
+                utils.handle_http_error(e, "Bad response during pagination")
+            except httpx.RequestError as e: 
+                utils.handle_http_error(e, "Network error occurred during pagination")
         logger.info("Pagination complete")
         return results
         
